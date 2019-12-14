c041af597ccf8aa1b004e420691e7fabaa353b86
rkaushik72 <rajiv_kaushik@yahoo.com>
2019-12-14 14:40:59 -0500

    cleanup more

--- utils.py
+++ utils.py
@@ -6,6 +6,9 @@ from gensim.utils import simple_preprocess
 from nltk.corpus import stopwords
 from textblob import TextBlob
 from contractions import CONTRACTION_MAP
+from nltk.corpus import sentiwordnet as swn
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
 
 # Initialize spacy 'en' model, keeping only tagger component (for efficiency)
 # python3 -m spacy download en
@@ -22,6 +25,9 @@ stop_words.remove('not')
 contraction_mapping = CONTRACTION_MAP
 contractions_pattern = re.compile('({})'.format('|'.join(contraction_mapping.keys())),
                                   flags=re.IGNORECASE | re.DOTALL)
+
+
+nltk.downloader.download('vader_lexicon')
 
 
 def multiprocNormalize(dflocal, sender, processName):
@@ -186,3 +192,83 @@ def print_topics_udf(topics, total_topics=1,
         print
 
 
+# below for unsupervised senti analysis ########
+
+def analyze_sentiment_sentiwordnet_lexicon_multiproc(reviews,sender,procIndex):
+
+    i=0
+    size=len(reviews)
+    sentiments=[]
+    sentiments.append(procIndex)
+    for review in reviews:
+        i=i+1
+        sentiment=analyze_sentiment_sentiwordnet_lexicon(review)
+        sentiments.append(sentiment)
+        if(i%1000 == 0):
+            print('process ' + str(procIndex)+' reached '+str(i) + ' of '+ str(size))
+
+    sender.send(sentiments)
+
+    return sentiments
+
+def analyze_sentiment_sentiwordnet_lexicon(review):
+    # tokenize and POS tag text tokens
+    tagged_text = [(token.text, token.tag_) for token in nlp(review)]
+    pos_score = neg_score = token_count = obj_score = 0
+    # get wordnet synsets based on POS tags
+    # get sentiment scores if synsets are found
+    for word, tag in tagged_text:
+        ss_set = None
+        if 'NN' in tag and list(swn.senti_synsets(word, 'n')):
+            ss_set = list(swn.senti_synsets(word, 'n'))[0]
+        elif 'VB' in tag and list(swn.senti_synsets(word, 'v')):
+            ss_set = list(swn.senti_synsets(word, 'v'))[0]
+        elif 'JJ' in tag and list(swn.senti_synsets(word, 'a')):
+            ss_set = list(swn.senti_synsets(word, 'a'))[0]
+        elif 'RB' in tag and list(swn.senti_synsets(word, 'r')):
+            ss_set = list(swn.senti_synsets(word, 'r'))[0]
+        # if senti-synset is found
+        if ss_set:
+            # add scores for all found synsets
+            pos_score += ss_set.pos_score()
+            neg_score += ss_set.neg_score()
+            obj_score += ss_set.obj_score()
+            token_count += 1
+
+    # aggregate final scores
+    final_score = pos_score - neg_score
+    norm_final_score = round(float(final_score) / token_count, 2)
+    final_sentiment = 1 if norm_final_score >= 0 else 0
+
+
+    return final_sentiment
+
+
+def analyze_sentiment_vader_multiproc(reviews,threshold,sender,procIndex):
+
+    i=0
+    size=len(reviews)
+    sentiments=[]
+    sentiments.append(procIndex)
+    for review in reviews:
+        i=i+1
+        sentiment=analyze_sentiment_vader_lexicon(review,threshold)
+        sentiments.append(sentiment)
+        if(i%1000 == 0):
+            print('process ' + str(procIndex)+' reached '+str(i) + ' of '+ str(size))
+
+    sender.send(sentiments)
+
+    return sentiments
+
+def analyze_sentiment_vader_lexicon(review,threshold):
+
+    # analyze the sentiment for review
+    analyzer = SentimentIntensityAnalyzer()
+    scores = analyzer.polarity_scores(review)
+    # get aggregate scores and final sentiment
+    agg_score = scores['compound']
+    final_sentiment = 1 if agg_score >= threshold \
+        else 0
+
+    return final_sentiment
