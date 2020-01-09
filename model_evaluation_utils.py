1898b3652615cf0e8444b3d57a2bf199c1723d16
rkaushik72 <rajiv_kaushik@yahoo.com>
2020-01-08 23:39:03 -0500

    tuning feature engg

--- model_evaluation_utils.py
+++ model_evaluation_utils.py
@@ -6,17 +6,18 @@ Created on Mon Jul 31 20:05:23 2017
 @Copyright: Dipanjan Sarkar
 """
 
-from sklearn import metrics
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder
-from sklearn.base import clone
-from sklearn.preprocessing import label_binarize
-from scipy import interp
-from sklearn.metrics import roc_curve, auc, confusion_matrix
-import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy import interp
+from sklearn import metrics
+from sklearn.base import clone
+from sklearn.metrics import roc_curve, auc, confusion_matrix
+from sklearn.model_selection import learning_curve
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import label_binarize
+
 
 def get_metrics(true_labels, predicted_labels):
     print('Accuracy:', np.round(
@@ -223,3 +224,27 @@ def plot_model_roc_curve(clf, features, true_labels, label_encoder=None, class_n
     plt.show()
 
 
+# Learning curve
+def learningCurve(X, Y, model, cv, train_sizes):
+    plt.figure()
+    plt.title("Learning curve")
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+
+    train_sizes, train_scores, test_scores = learning_curve(model, X, Y, cv=cv, n_jobs=4, train_sizes=train_sizes)
+
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.1,
+                     color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r", label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g", label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
